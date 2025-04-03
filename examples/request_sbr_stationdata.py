32ff923557b07ad4116b678909b8d53df3db6450
sitscholl <tscholl.simon@gmail.com>
2025-04-03 08:51:35 +0200

    finalize example of new SBR class based on requests

--- examples/request_sbr_stationdata.py
+++ examples/request_sbr_stationdata.py
@@ -8,7 +8,11 @@ parser.add_argument('password', help = 'password')
 args = parser.parse_args()
 
 with SBR(args.username, args.password) as client:
-    data = client.get_stationdata(station_id = '103', start=datetime.datetime(2025, 3, 31, 0, 0), end=datetime.datetime(2025, 4, 2, 14, 0))
-print(data.request.url)
-with open('fetch_response.html', "w", encoding = 'utf-8') as file: 
-    file.write(data.text)
+    data = client.get_stationdata(
+        station_id="103",
+        start=datetime.datetime(2025, 3, 31, 0, 0),
+        end=datetime.datetime(2025, 4, 2, 14, 0),
+        type = 'meteo'
+    )
+    
+print(data)
