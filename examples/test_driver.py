45487545fb7344aad84cd63eacb5bd378341e359
sitscholl <tscholl.simon@gmail.com>
2025-05-04 19:07:42 +0200

    delete download_dir afterwards

--- examples/test_driver.py
+++ examples/test_driver.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import time
+import shutil
 
 from webhandler.driver import Driver
 
@@ -7,9 +8,12 @@ download_dir = 'downloads'
 Path(download_dir).mkdir(parents = True, exist_ok=True)
 ddir_abs = str( Path.cwd() / download_dir ) #Make sure to use absolute path, otherwise it might fail 
 
-with Driver(download_dir=ddir_abs) as driver:
-    filename = "sample4.csv"
-    driver.get(f"https://filesamples.com/samples/document/csv/{filename}")
+try:
+    with Driver(download_dir=ddir_abs) as driver:
+        filename = "sample4.csv"
+        driver.get(f"https://filesamples.com/samples/document/csv/{filename}")
 
-    # Wait for the file to download, adjust if needed
-    time.sleep(1)
+        # Wait for the file to download, adjust if needed
+        time.sleep(1)
+finally:
+    shutil.rmtree(download_dir)
