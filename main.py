89bf88f50db68c3386b5d119f31b8b4c6300bbb4
sitscholl <tscholl.simon@gmail.com>
2025-09-11 19:55:30 +0200

    add logging config

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import uvicorn
 
+import logging
+import logging.config
+
 from webhandler.api import app
+
+# Configure logging
+logging.config.dictConfig(config['logging'])
 
 uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
