e9b3d475e2857faddc580a6fcc979cbfbe1b7f61
sitscholl <tscholl.simon@gmail.com>
2025-09-14 16:40:08 +0200

    import config

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import uvicorn
 import logging
 import logging.config
 
+from webhandler.config import load_config
 from webhandler.api import app
+
+# Load config file
+config = load_config("config/config.yaml")
 
 # Configure logging
 logging.config.dictConfig(config['logging'])
