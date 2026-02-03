e7a724bb85256f211fb0bc8f7012acae07b46795
sitscholl <tscholl.simon@gmail.com>
2026-02-03 20:44:34 +0100

    initial implementation of OpenMEteo handler

---
+++ src/meteo/open_meteo.py
@@ -0,0 +1,170 @@
+import pandas as pd
+import asyncio
+
+import logging
+from typing import Tuple, Dict, Any
+
+from .base import BaseMeteoHandler
+
+# a list of all the variables that are available
+# _HOURLY_VARIABLES = ["temperature_2m","relative_humidity_2m","dew_point_2m","apparent_temperature","precipitation_probability","precipitation","rain","showers","snowfall","snow_depth",
+#                     "soil_temperature_0cm","soil_temperature_6cm","soil_temperature_18cm","soil_temperature_54cm","soil_moisture_0_to_1cm","soil_moisture_1_to_3cm","soil_moisture_3_to_9cm","soil_moisture_9_to_27cm","soil_moisture_27_to_81cm",
+#                     "weather_code","pressure_msl","surface_pressure","cloud_cover","cloud_cover_low","cloud_cover_mid","cloud_cover_high","visibility","evapotranspiration","et0_fao_evapotranspiration","vapour_pressure_deficit",
+#                     "temperature_180m","temperature_120m","temperature_80m","wind_gusts_10m","wind_direction_180m","wind_direction_120m","wind_direction_80m","wind_direction_10m","wind_speed_180m","wind_speed_120m","wind_speed_80m","wind_speed_10m"]
+
+_OPENMETEO_HOURLY_RENAME = {
+    "time": "datetime",
+    "temperature_2m": "tair_2m",
+    "relative_humidity_2m": "relative_humidity",
+    "precipitation": "precipitation",
+    "wind_speed_10m": "wind_speed",
+    "wind_direction_10m": "wind_direction",
+    "wind_gusts_10m": "wind_gust",
+    "terrestrial_radiation_instant": "solar_radiation",
+    "snow_depth": "snow_height",
+}
+
+logger = logging.getLogger(__name__)
+
+class OpenMeteo(BaseMeteoHandler):
+    
+    provider_name = 'open-meteo'
+
+    base_url = "https://api.open-meteo.com/v1"
+    timeseries_url = base_url + "/forecast"
+
+    def __init__(self, locations: Dict[str, Dict], *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.locations = locations
+        self.variables = [i for i in _OPENMETEO_HOURLY_RENAME.keys() if i != 'time']
+
+    @property
+    def freq(self):
+        return "H"
+
+    @property
+    def inclusive(self):
+        """Must be one of 'left', 'right' or 'both'."""
+        return "both"
+
+    async def get_sensors(self, station_id: str) -> list[str]:
+        return list(self.variables)
+
+    async def get_stations(self) -> list[str] | None:
+        return list(self.locations.keys())
+
+    async def get_station_info(self, station_id: str | None) -> Dict[str, Any]:
+        if station_id is None:
+            return self.locations
+        return self.locations.get(station_id, {})
+
+    async def get_station_coords(self, station_id: str) -> Tuple[float, float]:
+        if station_id not in self.locations.keys():
+            raise ValueError(f"Station {station_id} not found. Choose one of {self.locations.keys()}")
+        info = self.locations[station_id]
+        return info['lat'], info['lon']
+
+    async def _create_request_task(
+        self, station_id: str, sensors: list[str]
+        ):
+
+        if self._client is None:
+            raise ValueError("Initialize client before requesting data")
+        
+        try:
+            lat, lon = await self.get_station_coords(station_id)
+            data_params = {
+                "latitude": lat,
+                "longitude": lon,
+                "hourly": sensors,
+                "timezone": self.timezone
+            }
+            async with self._semaphore:
+                response = await self._client.get(
+                        self.timeseries_url, params = data_params,
+                        timeout=self.timeout
+                    )
+                response.raise_for_status()
+                await asyncio.sleep(self.sleep_time)
+
+            response_data = pd.DataFrame(response.json()['hourly'])
+
+            if len(response_data) == 0:
+                logger.warning(f"No data found for {data_params}")
+                return None
+
+            response_data['station_id'] = station_id
+
+            return response_data
+        except Exception as e:
+            logger.error(f"Error fetching data for {station_id}: {e}", exc_info = True)
+            return None
+
+    async def get_raw_data(            
+            self,            
+            station_id: str,
+            sensor_codes: list[str] | None = None,
+            **kwargs
+        ) -> Tuple[pd.DataFrame | None, Dict]:
+
+        possible_stations = await self.get_stations()
+        if station_id not in possible_stations:
+            raise ValueError(f"Invalid station_id {station_id}. Choose one from {possible_stations}")
+        
+        all_sensors = await self.get_sensors()
+        if sensor_codes is None:
+            sensor_codes = all_sensors
+        else:
+            if not isinstance(sensor_codes, list):
+                raise ValueError(f"Sensor_codes must be of type list. Got {type(sensor_codes)}")
+            for sensor in sensor_codes:
+                if sensor not in all_sensors:
+                    raise ValueError(f"Invalid sensor {sensor}. Choose from: {all_sensors}")
+
+        # Create tasks
+        raw_response = await self._create_request_task(station_id, sensor_codes)        
+        st_metadata = await self.get_station_info(station_id)
+        if raw_response is None:
+            logger.warning(f"No data could be fetched for station {station_id}")
+
+        return raw_response, st_metadata
+
+    def transform(self, raw_data: pd.DataFrame | None) -> pd.DataFrame | None:
+
+        if raw_data is None:
+            return None
+
+        df_prepared = raw_data.copy()
+        if df_prepared[['time', 'station_id']].duplicated().any():
+            logger.warning("Found duplicates for ['time', 'station_id']. They will be dropped")
+            df_prepared.drop_duplicates(subset = ['time', 'station_id'], inplace = True)
+        
+        df_prepared.rename(columns = _OPENMETEO_HOURLY_RENAME, inplace = True)
+
+        for col in ['wind_direction', "relative_humidity"]:
+            if col in df_prepared.columns:
+                df_prepared[col] = df_prepared[col].astype(float)
+
+        try:
+            df_prepared['datetime'] = pd.to_datetime(df_prepared['datetime']).dt.tz_localize(self.timezone)
+            df_prepared['datetime'] = df_prepared['datetime'].dt.tz_convert('UTC')
+            df_prepared['datetime'] = df_prepared['datetime'].dt.floor(self.freq)
+        except Exception as e:
+            logger.error(f"Error transforming datetime: {e}")
+
+        return df_prepared
+
+if __name__ == '__main__':
+
+    async def test_fn():
+        locations = {'bozen': {'lat': 46.498, 'lon': 11.354}}
+        open_meteo = OpenMeteo(timezone = 'Europe/Rome', locations = locations)
+        async with open_meteo as prv:
+            data, _ = await prv.get_raw_data('bozen')
+        
+        transformed_data = open_meteo.transform(data)
+        validated_data = open_meteo.validate(transformed_data)
+        return validated_data
+
+    data = asyncio.run(test_fn())
+    print(data)
