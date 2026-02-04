8726b16fa260568c47e75c2e09625e52a5a64757
sitscholl <tscholl.simon@gmail.com>
2026-02-04 09:32:52 +0100

    handle models and column names from open meteo

--- src/meteo/open_meteo.py
+++ src/meteo/open_meteo.py
@@ -4,14 +4,18 @@ import asyncio
 import logging
 from typing import Tuple, Dict, Any
 import datetime
+import re
 
 from .base import BaseMeteoHandler
 
 # a list of all the variables that are available
+# Not used, insteal keys from _OPENMETEO_HOURLY_RENAME are used as possible variables
 # _HOURLY_VARIABLES = ["temperature_2m","relative_humidity_2m","dew_point_2m","apparent_temperature","precipitation_probability","precipitation","rain","showers","snowfall","snow_depth",
 #                     "soil_temperature_0cm","soil_temperature_6cm","soil_temperature_18cm","soil_temperature_54cm","soil_moisture_0_to_1cm","soil_moisture_1_to_3cm","soil_moisture_3_to_9cm","soil_moisture_9_to_27cm","soil_moisture_27_to_81cm",
 #                     "weather_code","pressure_msl","surface_pressure","cloud_cover","cloud_cover_low","cloud_cover_mid","cloud_cover_high","visibility","evapotranspiration","et0_fao_evapotranspiration","vapour_pressure_deficit",
 #                     "temperature_180m","temperature_120m","temperature_80m","wind_gusts_10m","wind_direction_180m","wind_direction_120m","wind_direction_80m","wind_direction_10m","wind_speed_180m","wind_speed_120m","wind_speed_80m","wind_speed_10m"]
+
+_OPENMETEO_WEATHER_MODELS = ["best_match", "ecmwf_ifs", "ecmwf_ifs025", "ecmwf_aifs025_single", "cma_grapes_global", "bom_access_global", "icon_seamless", "icon_global", "icon_eu", "icon_d2", "metno_seamless", "metno_nordic", "gfs_seamless", "gfs_global", "gfs_hrrr", "ncep_nbm_conus", "ncep_nam_conus", "gfs_graphcast025", "ncep_aigfs025", "ncep_hgefs025_ensemble_mean", "gem_seamless", "gem_global", "gem_regional", "gem_hrdps_continental", "gem_hrdps_west", "knmi_seamless", "knmi_harmonie_arome_europe", "knmi_harmonie_arome_netherlands", "dmi_seamless", "dmi_harmonie_arome_europe", "jma_seamless", "jma_msm", "jma_gsm", "meteofrance_seamless", "meteofrance_arpege_world", "meteofrance_arpege_europe", "meteofrance_arome_france", "meteofrance_arome_france_hd", "ukmo_seamless", "ukmo_global_deterministic_10km", "ukmo_uk_deterministic_2km", "kma_seamless", "kma_ldps", "kma_gdps", "italia_meteo_arpae_icon_2i", "meteoswiss_icon_seamless", "meteoswiss_icon_ch1", "meteoswiss_icon_ch2"]
 
 _OPENMETEO_HOURLY_RENAME = {
     "time": "datetime",
@@ -38,6 +42,8 @@ class OpenMeteo(BaseMeteoHandler):
         super().__init__(*args, **kwargs)
         self.locations = locations
         self.variables = [i for i in _OPENMETEO_HOURLY_RENAME.keys() if i != 'time']
+        self.models = list(_OPENMETEO_WEATHER_MODELS)
+        self._last_queried_models = None
 
     @property
     def freq(self):
@@ -54,6 +60,9 @@ class OpenMeteo(BaseMeteoHandler):
     async def get_stations(self) -> list[str] | None:
         return list(self.locations.keys())
 
+    async def get_models(self) -> list[str]:
+        return list(self.models)
+
     async def get_station_info(self, station_id: str | None) -> Dict[str, Any]:
         if station_id is None:
             return self.locations
@@ -66,7 +75,7 @@ class OpenMeteo(BaseMeteoHandler):
         return info['lat'], info['lon']
 
     async def _create_request_task(
-        self, station_id: str, sensors: list[str], start=None, end=None
+        self, station_id: str, models: list[str], sensors: list[str], start=None, end=None
         ):
 
         if self._client is None:
@@ -78,7 +87,8 @@ class OpenMeteo(BaseMeteoHandler):
                 "latitude": lat,
                 "longitude": lon,
                 "hourly": sensors,
-                "timezone": self.timezone
+                "timezone": self.timezone,
+                "models": models
             }
             if start is not None:
                 data_params["start_date"] = pd.Timestamp(start).strftime("%Y-%m-%d")
@@ -111,13 +121,24 @@ class OpenMeteo(BaseMeteoHandler):
             start: datetime.datetime | None = None,
             end: datetime.datetime | None = None,
             sensor_codes: list[str] | None = None,
+            models: str | list[str] = "meteoswiss_icon_seamless",
             **kwargs
         ) -> Tuple[pd.DataFrame | None, Dict]:
 
         possible_stations = await self.get_stations()
         if station_id not in possible_stations:
             raise ValueError(f"Invalid station_id {station_id}. Choose one from {possible_stations}")
-        
+
+        self._last_queried_models = None
+        if isinstance(models, str):
+            models = [models]
+        models = [m.lower() for m in models]
+
+        possible_models = await self.get_models()
+        invalid_models = [i for i in models if i not in possible_models]
+        if invalid_models:
+            raise ValueError(f"Invalid models {invalid_models}. Choose from {possible_models}")
+
         all_sensors = await self.get_sensors(station_id)
         if sensor_codes is None:
             sensor_codes = all_sensors
@@ -133,12 +154,13 @@ class OpenMeteo(BaseMeteoHandler):
 
         # Create tasks
         raw_response = await self._create_request_task(
-            station_id, sensor_codes, start=start, end=end
+            station_id, models, sensor_codes, start=start, end=end
         )
         st_metadata = await self.get_station_info(station_id)
         if raw_response is None:
             logger.warning(f"No data could be fetched for station {station_id}")
 
+        self._last_queried_models = models
         return raw_response, st_metadata
 
     def transform(self, raw_data: pd.DataFrame | None) -> pd.DataFrame | None:
@@ -147,11 +169,34 @@ class OpenMeteo(BaseMeteoHandler):
             return None
 
         df_prepared = raw_data.copy()
+
         if df_prepared[['time', 'station_id']].duplicated().any():
             logger.warning("Found duplicates for ['time', 'station_id']. They will be dropped")
             df_prepared.drop_duplicates(subset = ['time', 'station_id'], inplace = True)
-        
-        df_prepared.rename(columns = _OPENMETEO_HOURLY_RENAME, inplace = True)
+
+        # Rename columns, handling multi-model suffixes if present
+        new_columns: dict[str, str] = {}
+        models = self._last_queried_models or []
+        for col in df_prepared.columns:
+            match_found = False
+            for m in sorted(models, key=len, reverse=True):
+                if col.endswith(f"_{m}"):
+                    base = col[: -len(m) - 1]
+                    if base in _OPENMETEO_HOURLY_RENAME:
+                        new_columns[col] = f"{_OPENMETEO_HOURLY_RENAME[base]}_{m}"
+                        match_found = True
+                        break
+                    else:
+                        logger.warning(
+                            f"Unrecognized Open-Meteo parameter '{base}' for model '{m}'. Keeping original column '{col}'."
+                        )
+                        match_found = True
+                        break
+            if not match_found and col in _OPENMETEO_HOURLY_RENAME:
+                new_columns[col] = _OPENMETEO_HOURLY_RENAME[col]
+
+        if new_columns:
+            df_prepared.rename(columns=new_columns, inplace=True)
 
         for col in ['wind_direction', "relative_humidity"]:
             if col in df_prepared.columns:
@@ -172,7 +217,7 @@ if __name__ == '__main__':
         locations = {'bozen': {'lat': 46.498, 'lon': 11.354}}
         open_meteo = OpenMeteo(timezone = 'Europe/Rome', locations = locations)
         async with open_meteo as prv:
-            data, _ = await prv.get_raw_data('bozen')
+            data, _ = await prv.get_raw_data('bozen', models = ['meteoswiss_icon_seamless', 'best_match'])
         
         transformed_data = open_meteo.transform(data)
         validated_data = open_meteo.validate(transformed_data)
